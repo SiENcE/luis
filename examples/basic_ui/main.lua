3dbfe9f10b92048e58f596379dcc93d4a10d499e
SiENcE <seabeams@gmx.de>
2024-10-06 00:46:40 +0200

    basic_ui example with it own widgets added

---
+++ examples/basic_ui/main.lua
@@ -0,0 +1,46 @@
+local initLuis = require("luis.init")
+-- point this to your widgets folder
+local luis = initLuis("examples/basic_ui/widgets")
+
+local RetroMenu = require("examples.basic_ui.retro_menu")
+
+function love.load()
+    love.window.setMode(800, 600, {resizable=false, vsync=true})
+    
+    luis.initJoysticks()
+    luis.updateScale()
+    
+    RetroMenu.init()
+end
+
+function love.update(dt)
+    luis.update(dt)
+end
+
+function love.draw()
+    luis.draw()
+end
+
+function love.mousepressed(x, y, button, istouch)
+    luis.mousepressed(x, y, button, istouch)
+end
+
+function love.mousereleased(x, y, button, istouch)
+    luis.mousereleased(x, y, button, istouch)
+end
+
+function love.keypressed(key)
+    luis.keypressed(key)
+end
+
+function love.textinput(text)
+    luis.textinput(text)
+end
+
+function love.gamepadpressed(joystick, button)
+    luis.gamepadpressed(joystick, button)
+end
+
+function love.gamepadreleased(joystick, button)
+    luis.gamepadreleased(joystick, button)
+end
